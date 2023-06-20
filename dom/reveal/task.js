dceafadeca43eac66cf4ff7e9a37fd41298820f3
Dialuna <evashustova@gmail.com>
2023-06-20 14:46:20 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,16 @@
+const reveal = Array.from(document.querySelectorAll(".reveal"))
+
+const isVisible = () => {
+    reveal.forEach(item => {
+        const { top, bottom } = item.getBoundingClientRect()
+        if (top < 0) {
+            item.classList.add("reveal_active")
+        } else if (bottom < window.innerHeight) {
+            item.classList.add("reveal_active")
+        } else {
+            item.classList.remove("reveal_active")
+        }
+    })
+}
+
+window.addEventListener('scroll', isVisible)
